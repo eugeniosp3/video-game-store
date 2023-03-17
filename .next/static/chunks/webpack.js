d0b8e0d00d5feac0191537f58d9e52d949ecaa2d
ugino <gino@tendo.com>
2023-03-16 23:25:13 -0400

    fixed scrollbars

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7ea96136ee6c7360"; }
+/******/ 		__webpack_require__.h = function() { return "2ad7de38db29f021"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
